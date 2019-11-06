27a591cc03e9c627a3e6513998d4f9e68f1b538b
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-06 10:26:44 -0600

    Naive Component Approach

--- commentboard-project/src/index.js
+++ commentboard-project/src/index.js
@@ -4,7 +4,29 @@ import ReactDOM from 'react-dom';
 
 const App=()=>{
 
-  return <div>Peace</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            AbdulMalik
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>   
+            
+          </div>
+          <div className="text">Nice Blog Post Buddy.</div>
+
+        </div>
+
+      </div>
+    
+
+    </div>
+  )
 };
 
 
