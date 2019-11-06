7ee30521d3b2f7b2ce179f6b2cb71afb02f008b2
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-06 11:21:50 -0600

    Component Nesting

--- commentboard-project/src/index.js
+++ commentboard-project/src/index.js
@@ -1,73 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker'
+import faker from 'faker';
+import CommentDetail from './CommentDetail'
+import { cpus } from 'os';
 
 
 const App=()=>{
 
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            AbdulMalik
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>   
-            
-          </div>
-          <div className="text">Nice Blog Post Buddy.</div>
 
-        </div>
-
-      </div>
-
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sokhiba
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>   
-            
-          </div>
-          <div className="text">Nice Blog Post Buddy.</div>
-
-        </div>
-
-      </div>
-
-
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.avatar()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sumayya
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>   
-            
-          </div>
-          <div className="text">Nice Blog Post Buddy.</div>
-
-        </div>
-
-      </div>
+    <CommentDetail />
+    <CommentDetail />
+    < CommentDetail />
       
 
 
-     
+      
     </div>
-  )
+  );
 };
 
 
