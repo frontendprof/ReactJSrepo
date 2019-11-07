1b6cb74a975b8879913d7478e550c8c7db0d89d4
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-07 09:37:37 -0600

    Passing multiple props

--- commentboard-project/src/index.js
+++ commentboard-project/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
-import { cpus } from 'os';
+
 
 
 const App=()=>{
@@ -10,9 +10,24 @@ const App=()=>{
   return (
     <div className="ui container comments">
 
-    <CommentDetail author="AbdulMalik"/>
-    <CommentDetail author="Sokhiba"/>
-    < CommentDetail author="Sumayya"/>
+    <CommentDetail 
+    author="AbdulMalik" 
+    timeAgo="Today at 9:15AM" 
+    blogText="Some random generated blog post" 
+    blogImage={faker.image.avatar()}
+    />
+    <CommentDetail 
+     author="Sokhiba"
+     timeAgo="Today at 6:15PM"
+     blogText="Blog post about leading healthy life style"
+     blogImage={faker.image.avatar()}
+      />
+    < CommentDetail 
+    author="Sumayya"
+    timeAgo="Yesterday at 2:15AM"
+    blogText="Advanced aqida and arabic grammer classes"
+    blogImage={faker.image.avatar()}
+    />
       
 
 
