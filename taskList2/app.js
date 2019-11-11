0fd9d8dec3099497ff89e3bebea4f78a8d624a3f
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-11 15:30:21 -0600

    Clear tasks and filtering through tasks

--- taskList2/app.js
+++ taskList2/app.js
@@ -16,6 +16,12 @@ loadEventListeners();
 function loadEventListeners(){
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click',removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click',clearTasks);
+  // Filter task event
+  filter.addEventListener('keyup',filterTasks);
 } 
 
 // Add task
@@ -40,8 +46,52 @@ function addTask(e){
   li.appendChild(link);
   // Append li to ul
   taskList.appendChild(li);
-  console.log(li);
+
+  // Clear taskInput value
+  taskInput.value="";
+
   
 
   e.preventDefault();
+}
+
+// Remove task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm("Are you really sure about it?")){
+      e.target.parentElement.parentElement.remove();
+    }    
+  }  
+}
+
+
+// Clear tasks
+function clearTasks(){
+  if(confirm("You are deleting all tasks. Do you know about that?")){
+    // taskList.innerHTML="";
+
+    while(taskList.firstChild){
+      taskList.removeChild(taskList.firstChild);
+    }
+  }
+  
+}
+
+// Filter tasks
+function filterTasks(e){
+  const text=e.target.value.toLowerCase();
+
+
+  document.querySelectorAll('.collection-item').forEach(
+    function(task){
+      const item=task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text)!=-1){
+        task.style.display='block';
+      }
+      else{
+        task.style.display='none';
+      }
+    }
+  );
+
 }
