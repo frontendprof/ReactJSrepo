11a6c4e748fc78f2ce2306e9013d66b32ad12e16
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-11 00:59:12 -0600

    Initializing and scaffolding app component for new season project

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const SeasonDisplay=()=>{
+  return <div>Assalaam aleikom</div>
+
+};
+
+export default SeasonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const App=()=>{
+  return <div>Peace</div>
+
+};
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
