3e0de2235f8b4e65fd37ea70fb1fe418b51ff50d
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-11 14:20:59 -0600

    Working on duplicate

--- taskList/app.js
+++ taskList/app.js
@@ -22,5 +22,28 @@ function addTask(e){
   if(taskInput.value===""){
     alert("Add a task, you knucklehead");
   }
+  //Create li element
+  const li=document.createElement('li');
+  // Add class
+  li.className='collection-item';
+  // Create text node and append it to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  //Create link element
+  const link=document.createElement('a');
+  // Add class
+  link.className='delete-item secondary-content';
+  // Add icon html
+  link.innerHTML='<i class="fa fa-remove"></i>';
+  // Append it to li
+  li.appendChild(link);
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear task input value
+  taskInput.value="";
+
+
+
+
   e.preventDefault();
 }
