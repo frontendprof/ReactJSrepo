a0cab250bfbdff42f4e87af6a5786c0e94baea16
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-12 16:03:44 -0600

    Defining UI variables

---
+++ taskList3/app.js
@@ -0,0 +1,7 @@
+// Define UI Vars
+const form=document.querySelector("#task-form");
+const taskInput=document.querySelector('#task');
+const filter=document.querySelector("#filter");
+const taskList=document.querySelector('.collection');
+const clearBtn=document.querySelector(".clear-tasks");
+
