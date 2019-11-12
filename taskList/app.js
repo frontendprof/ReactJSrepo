a2c94221597cbe5931c55a9ad12d787ed6685d0e
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-11 22:54:09 -0600

    Displaying value to console from LS

--- taskList/app.js
+++ taskList/app.js
@@ -45,13 +45,23 @@ function addTask(e){
   // Append li to ul
   taskList.appendChild(li);
 
+  // Store in LS
+  storeInLocalStorage(taskInput.value);
+
   // Clear task input value
   taskInput.value="";
-
-
-
-
   e.preventDefault();
+}
+// Create Local Storage
+function storeInLocalStorage(task){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+  }else{
+    tasks=JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+  localStorage.setItem('tasks',JSON.stringify(tasks));
 }
 
 function removeTask(e){
