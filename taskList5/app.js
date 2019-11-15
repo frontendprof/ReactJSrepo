45fa551d4b32c261ce6a49846ff1b595de631415
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-15 04:25:28 -0600

    All event listeners are staged

--- taskList5/app.js
+++ taskList5/app.js
@@ -65,7 +65,20 @@ function clearTasks(){
 }
 
 // Filter tasks
-function filter
+function filterTasks(e){
+  const text=e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+    item=task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text)!=-1){
+      task.style.display='block';
+    }else{
+      task.style.display='none';
+    }
+  });
+
+}
 
 
 
