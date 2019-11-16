0f3ffa5b32e095709d804ba2fe4b32d9712687ca
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-11-16 04:20:36 -0600

    Storing in local storage

--- taskList3/app.js
+++ taskList3/app.js
@@ -43,9 +43,26 @@ function addTask(e){
   //Append li to ul
   taskList.appendChild(li);
 
+  // Store in LS
+  storeInLocalStorage(taskInput.value);
+
   // Clear taskInput
   taskInput.value="";
   e.preventDefault();
+}
+
+// Store in LS
+function storeInLocalStorage(task){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+  }else{
+    tasks=JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+
+  localStorage.setItem('tasks',JSON.stringify(tasks));
+
 }
 
 // Remove task
