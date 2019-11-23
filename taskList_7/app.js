34ee14fd235a5b6ff7d93984374de03ec67e5eb2
AbdulMalik <abdumaliksharipov@gmail.com>
2019-11-23 04:18:43 -0600

    Finalizing task list project

--- taskList_7/app.js
+++ taskList_7/app.js
@@ -107,7 +107,29 @@ function storeToLocalStorage(task){
 function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         e.target.parentElement.parentElement.remove();
+
+        // Remove from LS
+        removeFromLocalStorage(e.target.parentElement.parentElement);
     }
+
+}
+
+// Remove from LS
+function removeFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+
+    }
+    tasks.forEach(function(task,index){
+            if(taskItem.textContent===task){
+                tasks.splice(index,1);
+
+            }
+        });
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 
 }
 
@@ -118,7 +140,14 @@ function clearTasks() {
         taskList.removeChild(taskList.firstChild);
                
     }
+    // Clearing from local storage
+    clearFromLocalStorage();
     
+}
+
+// Clear from LS
+function clearFromLocalStorage(){
+    localStorage.clear();
 }
 
 // Filter tasks
