64e0b6ca1c8a72736a92e2b298bef9b4971a9eed
AbdulMalik <abdumaliksharipov@gmail.com>
2019-11-24 04:09:20 -0600

    Finalize

--- taskList8/app.js
+++ taskList8/app.js
@@ -111,7 +111,29 @@ function storeToLocalStorage(task){
 function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         e.target.parentElement.parentElement.remove();
+
+        // Remove from LS
+        removeFromLocalStorage(e.target.parentElement.parentElement);
     }
+
+}
+
+// Remove from LS
+function removeFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+
+    }
+    tasks.forEach(function(task,index){
+            if(taskItem.textContent===task){
+                tasks.splice(index,1);
+
+            }
+        });
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 
 }
 
@@ -121,8 +143,14 @@ function clearTasks(){
         taskList.removeChild(taskList.firstChild);
     
     }
+    clearTasks();
 }
 
+
+// Clear tasks
+function clearTasks(){
+    localStorage.clear();
+}
 // Filter task
 function filterTask(e){
     text=e.target.value.toLowerCase();
