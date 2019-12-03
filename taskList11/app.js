c95c3420653adbd5339ae8f082d69d4fe53777c5
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-03 00:46:13 -0600

    Remove list from local storage

--- taskList11/app.js
+++ taskList11/app.js
@@ -111,7 +111,25 @@ function storeToLocalStorage(task){
 function removeTask(e){
     if(e.target.parentElement.classList.contains('delete-item')){
         e.target.parentElement.parentElement.remove();
+
+        removeFromLocalStorage(e.target.parentElement.parentElement);
     }
+
+}
+// Remove from LS
+function removeFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks')===null){
+        tasks=[];
+    }else{
+        tasks=JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task,index){
+        if(taskItem.textContent===task){
+            tasks.splice(index,1);
+        }
+    });
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 
 }
 
