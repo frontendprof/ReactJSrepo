1f1dfaabe5dae0bfc1cadc260ba3d67453032b12
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-24 21:46:33 -0600

    Completion of proper rendering of conditionals

--- numPick3/index.js
+++ numPick3/index.js
@@ -5,12 +5,22 @@ function getNum(){
 class NumPicker extends React.Component{
     render(){
         const num=getNum();
+        let msg;
+        if(num===7){
+            msg=<div>
+                <h2>Congratulations, you got it</h2>
+                <img src="https://media.giphy.com/media/nXxOjZrbnbRxS/giphy.gif" />
+            </div>
+            
+        }else{
+            msg=<h2>Try again</h2>
+
+        }
         return(
             <div>
                 <h2>Your number is:</h2>
                 <h1>{num}</h1>
-                <h3>{num===7?"Congratulations, you got it":"Try again"}</h3>
-
+                {msg}
             </div>
 
         );
