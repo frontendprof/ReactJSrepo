57136c04b46d91d1343f9eca428b0eec4eddcc62
AbdulMalik <abdumaliksharipov@gmail.com>
2019-12-24 22:28:11 -0600

    Iniializing mood project afresh

---
+++ moodFinder/app.js
@@ -0,0 +1,19 @@
+
+function getMood(){
+    const moods=["happy","upset","sad","worried","paranoid","relaxed","careless","frightened"]
+    let randomMood=Math.floor(Math.random()*moods.length);
+    return moods[randomMood];
+}
+class MoodFinder extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Your current mood is:</h2>
+                <h1>{getMood()}</h1>
+                <h2>And you look like like the image below</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<MoodFinder  />, document.querySelector("#root"));
